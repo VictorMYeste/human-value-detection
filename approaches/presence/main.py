cf1bfd84c7fca6e47fb4c807519f6a7e97a9ffed
VictorMYeste <victormyeste@gmail.com>
2025-01-23 10:19:12 +0100

    Adjust optuna to try avoid overfitting

--- approaches/presence/main.py
+++ approaches/presence/main.py
@@ -30,11 +30,13 @@ def main() -> None:
 
     def objective(trial):
         # Suggest hyperparameters
-        learning_rate = trial.suggest_float("learning_rate", 1e-6, 5e-5, log=True)
+        """
         num_train_epochs = trial.suggest_int("num_train_epochs", 3, 10)
         batch_size = trial.suggest_categorical("batch_size", [2, 4])
         gradient_accumulation_steps = 2 if batch_size == 4 else 4
-        weight_decay = trial.suggest_float("weight_decay", 1e-8, 1e-2, log=True)
+        """
+        learning_rate = trial.suggest_float("learning_rate", 5e-6, 5e-5, log=True)
+        weight_decay = trial.suggest_float("weight_decay", 0.1, 0.3, log=True)
 
         # Run training with these hyperparameters
         trainer = run_training(
@@ -50,12 +52,13 @@ def main() -> None:
             model_directory=args.model_directory,
             multilayer=args.multilayer,
             slice_data=args.slice,
-            batch_size=batch_size,
-            num_train_epochs=num_train_epochs,
+            batch_size=4,
+            num_train_epochs=10,
             learning_rate=learning_rate,
             weight_decay=weight_decay,
-            gradient_accumulation_steps=gradient_accumulation_steps,
-            custom_stopwords = model_config["custom_stopwords"]
+            gradient_accumulation_steps=4,
+            early_stopping_patience=4,
+            #custom_stopwords = model_config["custom_stopwords"]
         )
 
         # Evaluate and return metric
@@ -66,7 +69,7 @@ def main() -> None:
     # If user passes --optimize, run Optuna optimization
     if args.optimize:
         study = optuna.create_study(direction="maximize")
-        study.optimize(objective, n_trials=30)
+        study.optimize(objective, n_trials=50)
         logger.info(f"Best value: {study.best_value}")
         logger.info(f"Best params: {study.best_params}")
 
@@ -92,9 +95,10 @@ def main() -> None:
             batch_size=4,
             num_train_epochs=10,
             learning_rate=2e-05,
-            weight_decay=0.01,
+            weight_decay=0.1,
             gradient_accumulation_steps=4,
-            early_stopping_patience=4
+            early_stopping_patience=4,
+            #custom_stopwords = model_config["custom_stopwords"]
         )
 
 if __name__ == "__main__":
